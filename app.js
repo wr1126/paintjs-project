90bbec1eb045b22c14687fa87039ef58f02520d5
sinsin <wr1126@naver.com>
2022-02-12 11:21:16 +0900

    third-2

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const canvas = document.querySelector("canvas");
+const {width, height} = canvas.getBoundingClientRect(); //viewport를 위치로 한 위치값 가져오는 매서드
 const palette = document.querySelector('.palette');
 const paletteWidthSize = palette.clientWidth;
 const paletteHeightSize = palette.clientHeight;
@@ -8,10 +9,12 @@ const eraseBtn = document.getElementById("Erase");
 const saveBtn = document.getElementById("Save");
 const shareBtn = document.getElementById("Share");
 
+canvas.width = width;
+canvas.height = height;
 ctx.fillStyle = "white";
-ctx.fillRect(0, 0, paletteWidthSize, paletteHeightSize);
+ctx.fillRect(0, 0, width, height);
 ctx.strokeStyle = "black";
-ctx.fillStyle = "green";
+ctx.fillStyle = "black";
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -28,6 +31,7 @@ function appendList(event){
         ul.append(li);        
         let random = "#" + Math.floor(Math.random() * 16777215).toString(16); 
         li.style.backgroundColor = random;
+        li.classList.add("colors");
     }
 }
 
@@ -50,12 +54,12 @@ function erase(event){
         eraseBtn.innerText = "Delete";
     }
 }
-
-////////////////////
-
+////////////////////////////////////////
+////////////////////////////////////////
+////////////////////////////////////////
 function canvasClick(event){
     if(filling){
-        ctx.fillRect(0, 0, paletteWidthSize, paletteHeightSize);
+        ctx.fillRect(0, 0, width, height);
     }
 }
 
@@ -79,7 +83,17 @@ function mouseMove(event){
         ctx.stroke();
     }
 }
+////////////////////////////////////////
+////////////////////////////////////////
+////////////////////////////////////////
 
+function colorClick(event){
+    alert("booyah");
+}
+
+////////////////////////////////////////
+////////////////////////////////////////
+////////////////////////////////////////
 if(canvas){
     canvas.addEventListener("mousemove", mouseMove); //마우스 움직일 시
     canvas.addEventListener("mousedown", startPainting); //마우스 클릭하고 있는 상태
@@ -87,9 +101,14 @@ if(canvas){
     canvas.addEventListener("mouseleave", stopPainting); //마우스가 캔버스 떠날 시
     canvas.addEventListener("click", canvasClick);
 }
+const colors = document.querySelectorAll(".colors");
+Array.from(colors).forEach(function(color){
+    color.addEventListener("click", colorClick);
+});
+
+/* Array.from(colors).forEach(color => color.addEventListener("click", colorClick)); */
 
 PaintBtn.addEventListener("click", fill);
 eraseBtn.addEventListener("click", erase);
-
 window.addEventListener("resize", appendList);
 appendList();
