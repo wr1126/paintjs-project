6d173ad9c1371106d607498e62ce63ce481a615d
sinsin <wr1126@naver.com>
2022-02-12 09:40:59 +0900

    third commit

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const canvas = document.querySelector("canvas");
+const palette = document.querySelector('.palette');
+const paletteWidthSize = palette.clientWidth;
+const paletteHeightSize = palette.clientHeight;
 const ctx = canvas.getContext("2d");
-const colors = "black";
-const range = "2.5";
-
-
-const fillBtn = document.getElementById("Fill");
+const PaintBtn = document.getElementById("Paint");
 const eraseBtn = document.getElementById("Erase");
 const saveBtn = document.getElementById("Save");
 const shareBtn = document.getElementById("Share");
 
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, paletteWidthSize, paletteHeightSize);
+ctx.strokeStyle = "black";
+ctx.fillStyle = "green";
+ctx.lineWidth = 2.5;
+
+let painting = false;
 let filling = false;
 let erasing = false;
 
 function appendList(event){
-    const palette = document.querySelector('.palette');
-    const paletteWidthSize = palette.clientWidth;
     const listWidthCount = Math.floor(paletteWidthSize / 70);
-    const paletteHeightSize = palette.clientHeight;
     const listHeightCount = Math.floor(paletteHeightSize / 70);
     const ul = document.getElementById("color-palette");
 
@@ -30,11 +33,11 @@ function appendList(event){
 
 function fill(event){
     if(filling){
-        filling = false;
-        fillBtn.innerText = "Fill";
+        filling = false; 
+        PaintBtn.innerText = "Paint";
     }else{
         filling = true;
-        fillBtn.innerText = "Faint";
+        PaintBtn.innerText = "Fill";
     }
 }
 
@@ -48,30 +51,44 @@ function erase(event){
     }
 }
 
-function startPainting(event){
-    ctx.beginPath();
-/*     ctx.fillStyle = "black";
-    ctx.fill(); */
+////////////////////
+
+function canvasClick(event){
+    if(filling){
+        ctx.fillRect(0, 0, paletteWidthSize, paletteHeightSize);
+    }
+}
+
+function startPainting(event){ 
+    painting = true;
 } 
 
 function stopPainting(event){
-
+    painting = false;
 }
 
-/* function canvasSize(){
-    const canvasSize = canvas.clientWidth * canvas.clientHeight;
-    console.log(canvasSize);
+function mouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY;
 
-} */
+    if(!painting){ //마우스가 클릭되고 떼어진 상태
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    }else{ //마우스가 클릭되어진 상태, 페인트 모드
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
 if(canvas){
-/*     casnvasSize(); */
-    canvas.addEventListener("mousemove", startPainting);
-    canvas.addEventListener("mousedown", startPainting);
-    canvas.addEventListener("mouseup", stopPainting);
-    canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("mousemove", mouseMove); //마우스 움직일 시
+    canvas.addEventListener("mousedown", startPainting); //마우스 클릭하고 있는 상태
+    canvas.addEventListener("mouseup", stopPainting);    //마우스 클릭 중단 시
+    canvas.addEventListener("mouseleave", stopPainting); //마우스가 캔버스 떠날 시
+    canvas.addEventListener("click", canvasClick);
 }
 
-fillBtn.addEventListener("click", fill);
+PaintBtn.addEventListener("click", fill);
 eraseBtn.addEventListener("click", erase);
 
 window.addEventListener("resize", appendList);
